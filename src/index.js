bfa1a28f0e02849fa74b76d30427b4bff860896c
Kevin Rossetti Fernandes <kevinrf100@gmail.com>
2022-08-09 20:59:09 -0300

    Get statement

--- src/index.js
+++ src/index.js
@@ -22,8 +22,20 @@ app.post("/account", (req, res) => {
         id: uuidV4(), 
         statement: []
     });
-    
+
     return res.status(200).json({message: 'Account created successfully!'})
+});
+
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+    
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({error: "Customer not found!"});
+    }
+
+    return res.status(200).json(customer.statement);
 });
 
 app.listen(3031);
